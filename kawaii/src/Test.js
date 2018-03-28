f086dd550d2593710e042e3589311267efaf7e9c
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-03-28 19:54:34 +0900

    test prettier

--- kawaii/src/Test.js
+++ kawaii/src/Test.js
@@ -1,10 +1,8 @@
-var aaaa =              'aaaaaa'
-
-
-foo({ aa: 111, bbb: 4234242,})
-
-foo(111,
-  {
-    hogehoge: fooo
-  }
+const aaa = 'aaaaaaaaaaaaaaaaaaaaaaa'
+foo(
+  reallyLongArg(),
+  omgSoManyParameters(),
+  IShouldRefactorThis(),
+  isThereSeriouslyAnotherOne()
 )
+console.log(aaa)
